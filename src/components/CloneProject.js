3640cf36155b80e741edd498cb87b1f9f0f1c006
jujuicy97 <jujuicy97@gmail.com>
2025-08-14 12:10:40 +0900

    0814 12:10

--- src/components/CloneProject.js
+++ src/components/CloneProject.js
@@ -7,6 +7,86 @@ const CloneProject = () => {
         <div>2</div>
         <div>3</div>
       </div>
+      <div className="cloneCard-wrap">
+      <div className="net">
+        <img src="./image/Clone/net-img.jpg"/>
+        <div className="net-txt">
+          <div className="net-txt-top">
+            <h1>넷플릭스 클론 사이트</h1>
+            <p>국내 대표 OTT스트리밍 사이트의 UI를 클론하여 사용자가 최신 인기 영화를 확인하며, 클릭 시 상세 정보를 확인하는 웹사이트를 구현하였습니다.</p>
+            <p>오픈API 활용법을 익혀 실제 데이터와 연동하는 경험을 쌓았으며, React기반 라이브러리로 UI를 구현해 개발 역량을 키웠습니다.</p>
+          </div>
+          <div className="net-txt-bottom">
+            <p>제작에 사용된 스킬</p>
+            <p>React, React-Router, SCSS</p>
+            <p>UI 디자인 방식</p>
+            <p>Figma를 활용한 디자인 시스템 구축 후 코드 구현</p>
+            <p>레이아웃 제작 방식</p>
+            <p>Flex와 Grid를 화용하여 콘텐츠를 정렬한 스크롤 반응형 레이아웃 제작</p>
+            <p>플러그인</p>
+            <p>React, React-router, React-icons, React-slick, TMDB API, SCSS</p>
+          </div>
+          <div className="net-txt-point">
+            <p>100%(개인프로젝트)</p>
+            <p>오픈API와 라이브러리 활용</p>
+            <a> https://jujuicy97.github.io/netflix/</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="mom">
+        <img src="./image/Clone/mom-img.jpg"/>
+        <div className="net-txt">
+          <div className="net-txt-top">
+            <h1>넷플릭스 클론 사이트</h1>
+            <p>국내 대표 OTT스트리밍 사이트의 UI를 클론하여 사용자가 최신 인기 영화를 확인하며, 클릭 시 상세 정보를 확인하는 웹사이트를 구현하였습니다.</p>
+            <p>오픈API 활용법을 익혀 실제 데이터와 연동하는 경험을 쌓았으며, React기반 라이브러리로 UI를 구현해 개발 역량을 키웠습니다.</p>
+          </div>
+          <div className="net-txt-bottom">
+            <p>제작에 사용된 스킬</p>
+            <p>React, React-Router, SCSS</p>
+            <p>UI 디자인 방식</p>
+            <p>Figma를 활용한 디자인 시스템 구축 후 코드 구현</p>
+            <p>레이아웃 제작 방식</p>
+            <p>Flex와 Grid를 화용하여 콘텐츠를 정렬한 스크롤 반응형 레이아웃 제작</p>
+            <p>플러그인</p>
+            <p>React, React-router, React-icons, React-slick, TMDB API, SCSS</p>
+          </div>
+          <div className="net-txt-point">
+            <p>100%(개인프로젝트)</p>
+            <p>오픈API와 라이브러리 활용</p>
+            <a> https://jujuicy97.github.io/netflix/</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="air">
+        <img src="./image/Clone/air-img.jpg"/>
+        <div className="net-txt">
+          <div className="net-txt-top">
+            <h1>넷플릭스 클론 사이트</h1>
+            <p>국내 대표 OTT스트리밍 사이트의 UI를 클론하여 사용자가 최신 인기 영화를 확인하며, 클릭 시 상세 정보를 확인하는 웹사이트를 구현하였습니다.</p>
+            <p>오픈API 활용법을 익혀 실제 데이터와 연동하는 경험을 쌓았으며, React기반 라이브러리로 UI를 구현해 개발 역량을 키웠습니다.</p>
+          </div>
+          <div className="net-txt-bottom">
+            <p>제작에 사용된 스킬</p>
+            <p>React, React-Router, SCSS</p>
+            <p>UI 디자인 방식</p>
+            <p>Figma를 활용한 디자인 시스템 구축 후 코드 구현</p>
+            <p>레이아웃 제작 방식</p>
+            <p>Flex와 Grid를 화용하여 콘텐츠를 정렬한 스크롤 반응형 레이아웃 제작</p>
+            <p>플러그인</p>
+            <p>React, React-router, React-icons, React-slick, TMDB API, SCSS</p>
+          </div>
+          <div className="net-txt-point">
+            <p>100%(개인프로젝트)</p>
+            <p>오픈API와 라이브러리 활용</p>
+            <a> https://jujuicy97.github.io/netflix/</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
     </div>
   );
 };
